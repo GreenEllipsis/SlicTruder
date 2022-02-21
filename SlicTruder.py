01f3a30ae4043305aae900a7497b14935896d85c
Brian Alano <brian@greenellipsis.org>
2022-02-20 22:14:38 -0500

    WIP

--- SlicTruder.py
+++ SlicTruder.py
@@ -2,16 +2,37 @@
 #Created by the Autotrude project https://wikifactory.com/@greenellipsis/autotrude/
 #Released under the terms of the Creative Commons International 4.0 Attribution license (CC BY 4.0)
 #Ver 0.1 added despool option
+import PySimpleGUI as sg
 
 #Change these slicer parameters as needed
-action=1              # 1 - pultrude, 2 - despool
+action=1            # 1 - pultrude, 2 - despool
 DespoolSpeed = 1200
-ExtruderSpeed = 300*0.55   # mm/min
-ExtruderDistance = 0.1  # per G-code command
+ExtruderSpeed = 325   # mm/min
+ExtruderDistance = 0.2  # per G-code command
 Etotal = 60000         # total amount to extrude, in mm
-NozzleTemp = 220          # nozzle temperature
+NozzleTemp = 235          # nozzle temperature
+NozzleTempPrime = 235     # nozzle temperature while priming
+PrimingDistance = 200     # mm
 start_gcode = "M302 P1 ;allow cold extrude\n" \
-              "M83 ;extruder relative mode\n"
+              "M83 ;extruder relative mode\n" \
+              "M405 ;filament width sensor on\n" \
+              "M503 E3000 ;max E feedrate\n" \
+              "M75 ; start job timer\n"
+stop_gcode = "M18 ;disable steppers\n" \
+             "M77 ; stop job timer\n" \
+            "M78 ; display jobs stats\n" \
+             "M300 ; play tone\n" \
+            "M117 T20 RECREATING FILAMENT\n" \
+                ""
+prime_end_gcode = "M0 ; wait for user\n"
+periodic_gcode = "M407\n"
+period = 5 #every 10 mm
+
+# main
+layout = [
+    [sg.T('')]
+]
+window
 
 def despool():
     fname= "DS" + str(Etotal) + "@F" + str(DespoolSpeed) + ".gcode"
@@ -23,27 +44,31 @@ def despool():
     while Eposition <= Etotal:
         f.write("G1 E-" + str(ExtruderDistance)+"\n")
         Eposition += ExtruderDistance
-    f.write("M300 ; play tone\n")
-    f.write("M18 ;disable steppers\n")
+    f.write(stop_gcode)
     f.close()
 
 
 def pultrude():
-    fname= str(Etotal) + "@F" + str(ExtruderSpeed) + "@S" + str(NozzleTemp) + ".gcode"
+    fname= f'gcode/{Etotal}@F{ExtruderSpeed}@S{NozzleTemp}.gcode'
+    print("Writing " + fname + "...\n")
     f= open(fname,"w+")
     f.write(start_gcode)
-    f.write("M109 S" + str(NozzleTemp) + "\n")
-    f.write("M117 RECREATING FILAMENT\n")
+    f.write("M109 S" + str(NozzleTempPrime) + "\n")
     f.write("G1 E0 F" + str(ExtruderSpeed) + " ;set default feed rate\n")
-    f.write("M83 ;extruder relative mode\n")
     Eposition = 0
+    donePriming = False
     while Eposition <= Etotal:
+        if not donePriming and Eposition > PrimingDistance :
+            donePriming = True
+            f.write("M104 S" + str(NozzleTemp) + "\n")
         f.write("G1 E" + str(ExtruderDistance)+"\n")
         Eposition += ExtruderDistance
-    f.write("M300 ; play tone\n")
+        if Eposition % period < ExtruderDistance:
+            f.write(periodic_gcode)
     f.write("M104 S0 ;turn off nozzle heater\n")
-    f.write("M18 ;disable steppers\n")
+    f.write(stop_gcode)
     f.close()
+    print("Done\n")
 
 if action==1:
         pultrude()
